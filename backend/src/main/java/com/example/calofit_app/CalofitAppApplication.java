2223a99755e9184bea1315e3e1eec51bef0692c3
trangpetty <trangpetty22072001@gmail.com>
2026-06-16 16:39:32 +0700

    create foudnation

---
+++ backend/src/main/java/com/example/calofit_app/CalofitAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.calofit_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalofitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalofitAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/calofit_app/CalofitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calofit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalofitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
