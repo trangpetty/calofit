318d65a2b30af45a534b7620d683602f50d3edb6
petty <trangpetty22072001@gmail.com>
2026-06-25 23:13:36 +0700

    SCRUM-45: create layout for landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
+import HeaderLayout from "@/app/components/layout/Header";
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <HeaderLayout />
+    </>
   );
 }
